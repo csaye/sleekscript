02f409773a706e0a0db38f0b94953af4fe88e7b2
Cooper Saye <coopersaye@gmail.com>
2021-07-09 21:19:15 -0700

    Create Editor component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,11 @@
+import Editor from '../Editor/Editor.js';
+
 import styles from './App.module.css';
 
 export default function App() {
   return (
     <div>
+      <Editor />
     </div>
   );
 }
---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/javascript/javascript.js';
+import { Controlled } from 'react-codemirror2';
+
+import styles from './Editor.module.css';
+
+export default function Editor() {
+  const [code, setCode] = useState('');
+
+  return (
+    <div>
+      <Controlled
+        value={code}
+        onBeforeChange={(e, d, v) => setCode(v)}
+        options={{
+          lineWrapping: true,
+          mode: 'javascript',
+          theme: 'material'
+        }}
+      />
+      <Controlled
+        value={code}
+        options={{
+          lineWrapping: true,
+          mode: 'javascript',
+          theme: 'material'
+        }}
+      />
+    </div>
+  );
+}
