b7a52ae2d53e5cf48d43e96eaca758ff49be94d9
Cooper Saye <coopersaye@gmail.com>
2021-07-11 10:47:58 -0700

    Implement slice syntax.

--- src/sleekscript/lexer.js
+++ src/sleekscript/lexer.js
@@ -3,7 +3,7 @@ const possibleTokens = [
   { type: 'comment', regex: /(?<=#).*/},
   { type: 'keyword', regex: /(and|or|not|if|else)(?=([^\w]|$))/},
   { type: 'string', regex: /"([^"\\]|\\.)*"/ },
-  { type: 'slice', regex: /(?<=\[)\w*:\w*:?\w*(?=\])/ },
+  { type: 'slice', regex: /(?<=\[)\w*:\w*(?=\])/ },
   { type: 'number', regex: /\d*\.?\d+/ },
   { type: 'word', regex: /[_A-Za-z]\w*/ },
   { type: 'operator', regex: /[-+/*%?:=(){};,.<>&|]/ },
--- src/sleekscript/parser.js
+++ src/sleekscript/parser.js
@@ -1,15 +1,33 @@
 // returns code snippet for given token
-function getCode(token) {
+function getCode(tokens, i) {
+  const token = tokens[i];
   // add content based on type and value
   switch (token.type) {
     case 'comment': return `//${token.value}`;
-    case 'keyword':
+    case 'keyword': {
       switch (token.value) {
         case 'and': return '&&';
         case 'or': return '||';
         case 'not': return '!';
         default: return token.value;
       }
+    }
+    case 'slice': {
+      const vals = token.value.split(':');
+      // array slice
+      if (i > 0 && tokens[i - 1].type === 'word') {
+        if (!vals[0] && !vals[1]) return '.split()';
+        else if (!vals[1]) return `.split(${vals[0]})`;
+        else return `.split(${vals[0] ? vals[0] : 0}, ${vals[1]})`;
+      // array range
+      } else {
+        const start = vals[0] ? vals[0] : 0;
+        const end = vals[1] ? vals[1] : 0;
+        const nums = [];
+        for (let i = start; i < end; i++) nums.push(i);
+        return `[${nums.join(', ')}]`;
+      }
+    }
     default: return token.value;
   }
 }
@@ -18,6 +36,8 @@ function getCode(token) {
 export default function parse(tokens) {
   let js = '';
   // append code for each token
-  for (const token of tokens) js += getCode(token);
+  for (let i = 0; i < tokens.length; i++) {
+    js += getCode(tokens, i);
+  }
   return js;
 }
