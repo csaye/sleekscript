308e5dff7412a52dedcc3f88e5b0ca90baa6d817
Cooper Saye <coopersaye@gmail.com>
2021-07-10 20:28:45 -0700

    Reduce to first matched token.

--- src/sleekscript/lexer.js
+++ src/sleekscript/lexer.js
@@ -1,17 +1,31 @@
 // recursively retrieves tokens from given code
 function getTokens(code, tokens) {
+  const matches = [];
   // for each possible token
   for (const token of possibleTokens) {
     // if code matches token
     const match = code.match(token.regex);
     if (match) {
-      // push token
-      tokens.push({
+      // push match
+      matches.push({
         match: match,
         type: token.type
       });
     }
   }
+  // if no more matches, return tokens
+  if (!matches.length) return tokens;
+  // get match with earliest index
+  const first = matches.reduce((acc, match) =>
+  (!acc || match.match['index'] < acc.match['index']) ? match : acc);
+  // push token
+  tokens.push({
+    type: first.type,
+    value: first.match[0]
+  });
+  // slice code from end of match and recurse
+  code = code.slice(first.match['index'] + first.match[0].length);
+  return getTokens(code, tokens);
 }
 
 // lexes given sleekscript into tokens
