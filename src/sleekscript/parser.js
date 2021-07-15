e286d7ecaabc4f76f34039ac40d30c9f3e9fecc3
Cooper Saye <coopersaye@gmail.com>
2021-07-14 22:22:51 -0700

    Improve value parsing.

--- src/sleekscript/parser.js
+++ src/sleekscript/parser.js
@@ -1,15 +1,30 @@
+function getValue(tokens) {
+  // validate tokens
+  for (const token of tokens) {
+    if (token.type === 'assignment') return `// value error: unexpected ${token.value}`;
+  }
+  if (tokens[0].type === 'operator') return `// value error: unexpected ${tokens[0].value}`;
+  if (tokens[tokens.length - 1].type === 'operator') return `// value error: unexpected ${tokens[tokens.length - 1].value}`;
+  // format tokens
+  let value = '';
+  for (const token of tokens) {
+    if (value !== '' && token.value !== ')' && value[value.length - 1] !== '(') value += ' ';
+    value += token.value;
+  }
+  return value;
+}
+
 // returns type of given statement
 function getStatementType(statement) {
   if (!statement.length) return 'empty';
   switch (statement[0].type) {
     case 'comment': return 'comment';
-    case 'assignment': return `unexpected ${statement[0].value}`
     case 'word': {
       if (statement.length < 3) return 'unfinished statement';
       if (statement[1].type !== 'assignment') return `unexpected ${statement[1].value}`;
       return 'assignment';
     }
-    default: return 'unknown';
+    default: return `unexpected ${statement[0].value}`
   }
 }
 
@@ -20,8 +35,8 @@ function getStatementCode(statement) {
   switch (type) {
     case 'empty': return '';
     case 'comment': return `//${statement[0].value.slice(1)}`;
-    case 'assignment': return `${statement.map(token => token.value).join(' ')};`
-    default: return `// error: ${type}`;
+    case 'assignment': return `${statement[0].value} ${statement[1].value} ${getValue(statement.slice(2))};`
+    default: return `// statement error: ${type}`;
   }
 }
 
