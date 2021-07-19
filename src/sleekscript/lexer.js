8540987f8a42317b4f7ba04b8409dff28c9a1f5f
Cooper Saye <coopersaye@gmail.com>
2021-07-18 19:17:37 -0700

    Simplify code lexing.

--- src/sleekscript/lexer.js
+++ src/sleekscript/lexer.js
@@ -1,49 +1,25 @@
-// possible tokens to match ordered by precedence
-const possibleTokens = [
-  { type: 'comment', regex: /#.*/},
-  { type: 'keyword', regex: /(is|isnt|and|or|not)(?=([^\w]|$))/ },
-  { type: 'string', regex: /"([^"\\]|\\.)*"/ },
-  { type: 'number', regex: /\d*\.?\d+/ },
-  { type: 'word', regex: /[_A-Za-z]\w*/ },
-  { type: 'assignment', regex: /[-+/*%]=|=/ },
-  { type: 'parentheses', regex: /[()]/ },
-  { type: 'operator', regex: /[-+/*%]/ },
-  { type: 'separator', regex: /[\n;]/ }
-];
+// globals
+let index = 0;
+let code = undefined;
+let tokens = [];
 
-// recursively retrieves tokens from given code
-function getTokens(code, tokens) {
-  const matches = [];
-  // for each possible token
-  for (const token of possibleTokens) {
-    // if code matches token
-    const match = code.match(token.regex);
-    if (match) {
-      // push match
-      matches.push({
-        match: match,
-        type: token.type
-      });
-    }
-  }
-  // if no more matches, return tokens
-  if (!matches.length) return tokens;
-  // get match with earliest index
-  const first = matches.reduce((acc, match) =>
-  (!acc || match.match['index'] < acc.match['index']) ? match : acc);
-  // push token
-  tokens.push({
-    type: first.type,
-    value: first.match[0]
-  });
-  // slice code from end of match and recurse
-  code = code.slice(first.match['index'] + first.match[0].length);
-  return getTokens(code, tokens);
+// processes given character in token context
+function processChar(char) {
 }
 
 // lexes given sleekscript into tokens
-export default function lex(code) {
-  const tokens = [];
-  getTokens(code, tokens); // recursively retrieve tokens
+export default function lex(inputCode) {
+  // set globals
+  index = 0;
+  code = inputCode;
+  tokens = [];
+  // read to end of code
+  while (index < code.length) {
+    // process character
+    const char = code[index];
+    processChar(char);
+    index += 1;
+  }
+  // return lexed tokens
   return tokens;
 }
